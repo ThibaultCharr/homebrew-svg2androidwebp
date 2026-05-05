394dcde1a6790a70ff9eee6b43ad39bc070d05ad
thibault.charriere <thibault.charriere@adevinta.com>
2026-05-05 16:01:08 +0200

    fix: correct SHA256 checksums and package URLs

--- Formula/svg2androidwebp.rb
+++ Formula/svg2androidwebp.rb
@@ -6,36 +6,36 @@ class Svg2androidwebp < Formula
   license "MIT"
 
   depends_on "python@3.12"
-  depends_on "libffi"
   depends_on "cairo"
+  depends_on "libffi"
 
   resource "cairosvg" do
-    url "https://files.pythonhosted.org/packages/source/c/cairosvg/cairosvg-2.7.1.tar.gz"
-    sha256 "432531d72f9f73e1f53ca55b98f2a1be68af2e7a547bc7ac09e5dd9ff29a53af"
+    url "https://files.pythonhosted.org/packages/38/07/e8412a13019b3f737972dea23a2c61ca42becafc16c9338f4ca7a0caa993/cairosvg-2.9.0.tar.gz"
+    sha256 "1debb00cd2da11350d8b6f5ceb739f1b539196d71d5cf5eb7363dbd1bfbc8dc5"
   end
 
   resource "Pillow" do
-    url "https://files.pythonhosted.org/packages/source/p/pillow/pillow-10.4.0.tar.gz"
-    sha256 "166c1cd4d24309b30d61f79f4a9114b7b2313d7450a3df4beaf4de34b2ee0f7b"
-  end
-
-  resource "cssselect2" do
-    url "https://files.pythonhosted.org/packages/source/c/cssselect2/cssselect2-0.7.0.tar.gz"
-    sha256 "1ccd984dab89fc68955043aca4e1b03e0cf29cad9880f6e28e3ba7a74b14aa5a"
+    url "https://files.pythonhosted.org/packages/8c/21/c2bcdd5906101a30244eaffc1b6e6ce71a31bd0742a01eb89e660ebfac2d/pillow-12.2.0.tar.gz"
+    sha256 "a830b1a40919539d07806aa58e1b114df53ddd43213d9c8b75847eee6c0182b5"
   end
 
   resource "tinycss2" do
-    url "https://files.pythonhosted.org/packages/source/t/tinycss2/tinycss2-1.3.0.tar.gz"
-    sha256 "152f9acabd296a8375fbca5b84c961ff95971fcfc543d14e43dc61b37c3a2e6e"
+    url "https://files.pythonhosted.org/packages/a3/ae/2ca4913e5c0f09781d75482874c3a95db9105462a92ddd303c7d285d3df2/tinycss2-1.5.1.tar.gz"
+    sha256 "d339d2b616ba90ccce58da8495a78f46e55d4d25f9fd71dfd526f07e7d53f957"
+  end
+
+  resource "cssselect2" do
+    url "https://files.pythonhosted.org/packages/e0/20/92eaa6b0aec7189fa4b75c890640e076e9e793095721db69c5c81142c2e1/cssselect2-0.9.0.tar.gz"
+    sha256 "759aa22c216326356f65e62e791d66160a0f9c91d1424e8d8adc5e74dddfc6fb"
   end
 
   resource "defusedxml" do
-    url "https://files.pythonhosted.org/packages/source/d/defusedxml/defusedxml-0.7.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/0f/d5/c66da9b79e5bdb124974bfe172b4daf3c984ebd9c2a06e2b8a4dc7331c72/defusedxml-0.7.1.tar.gz"
     sha256 "1bb3032db185915b62d7c6209c5a8792be6a32ab2fedacc84e01b52c51aa3e69"
   end
 
   resource "webencodings" do
-    url "https://files.pythonhosted.org/packages/source/w/webencodings/webencodings-0.5.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/0b/02/ae6ceac1baeda530866a85075641cec12989bd8d31af6d5ab4a3e8c92f47/webencodings-0.5.1.tar.gz"
     sha256 "b36a1c245f2d304965eb4e0a82848379241dc04b865afcc4aab16748587e1923"
   end
 
