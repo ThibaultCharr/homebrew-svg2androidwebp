82f5e86e1a0e322808382a92cc671b304a903bcf
thibault.charriere <thibault.charriere@adevinta.com>
2026-05-05 16:13:30 +0200

    chore: bump to v1.7.0

--- Formula/svg2androidwebp.rb
+++ Formula/svg2androidwebp.rb
@@ -1,8 +1,8 @@
 class Svg2androidwebp < Formula
   desc "Convert SVG files to Android WebP density variants"
   homepage "https://github.com/ThibaultCharr/svg2androidwebp"
-  url "https://github.com/ThibaultCharr/svg2androidwebp/archive/refs/tags/v1.6.0.tar.gz"
-  sha256 "aafc32a874868e8f28bd5dc636e8e090ed4ec5ab7a45f22c37ea58e8fcd2ff26"
+  url "https://github.com/ThibaultCharr/svg2androidwebp/archive/refs/tags/v1.7.0.tar.gz"
+  sha256 "f61098f0072d8449121b512c1a2a397d4db151921500668b652d3033c9d71ead"
   license "MIT"
 
   depends_on "librsvg"
